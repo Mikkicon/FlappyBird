fc1ab40c0e82df43e84b5857a8a80114f7684076
Mikhail Petrenko <mikkicon@gmail.com>
2018-06-16 15:47:04 +0300

    First commit

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+    private BoxCollider2D groundcollider;
+    private float horizontallength;
+	// Use this for initialization
+	void Start () {
+        groundcollider = GetComponent<BoxCollider2D>();
+        horizontallength = groundcollider.size.x;
+	}
+    private void reposition(){
+        Vector2 offset = new Vector2(horizontallength *6f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        print("transform.position.x: "+ transform.position.x + "\n -horizontallength: "+-horizontallength);
+        if(transform.position.x < -horizontallength){
+            reposition();
+        }
+	}
+}
